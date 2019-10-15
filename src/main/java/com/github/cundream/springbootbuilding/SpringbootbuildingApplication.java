26109b2d1425759abde018c7e3c7e43453bdea76
licun <cundream@163.com>
2019-10-15 10:15:30 +0800

    20191015 init project

---
+++ src/main/java/com/github/cundream/springbootbuilding/SpringbootbuildingApplication.java
@@ -0,0 +1,13 @@
+package com.github.cundream.springbootbuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbuildingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootbuildingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/cundream/springbootbuilding/SpringbootbuildingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.cundream.springbootbuilding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootbuildingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
