2c9f95a6b9b1189975b37cce887c344df051067c
licun <cundream@163.com>
2019-10-16 11:45:40 +0800

    Spring Boot 工程系统构建 ----004 使用Druid和监控配置

---
+++ src/main/java/com/github/cundream/springbootbuilding/druid/filter/DruidStatFilter.java
@@ -0,0 +1,19 @@
+package com.github.cundream.springbootbuilding.druid.filter;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * @author : Lison
+ * @Date: 2019/10/16 11:32
+ * @Description: druid过滤器
+ */
+@WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
+        initParams={
+                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")//忽略资源
+        }
+)
+public class DruidStatFilter extends WebStatFilter {
+}
