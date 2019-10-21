c43a888c3bdc708f967753b8470fbf8009dd3863
licun <cundream@163.com>
2019-10-21 15:28:06 +0800

    Spring Boot 工程系统构建 ----010 整合Thymeleaf

---
+++ src/main/java/com/github/cundream/springbootbuilding/controller/ThymeleafController.java
@@ -0,0 +1,35 @@
+package com.github.cundream.springbootbuilding.controller;
+
+import com.github.cundream.springbootbuilding.service.UserService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Lison
+ * @Date: 2019/10/21 11:14
+ * @Description:
+ */
+@Controller
+@RequestMapping("/thymeleaf")
+@Api(value = "ThymeleafController", tags = "thymeleaf", description = "Thymeleaf测试接口")
+public class ThymeleafController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/userList",method = RequestMethod.GET)
+    public String userList(Model model){
+
+        model.addAttribute("users", userService.getUserById());
+        model.addAttribute("thymeleafName", "thymeleafName ----6666666");
+
+        return "userList";
+    }
+
+
+}
